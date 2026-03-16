6128729cb01e50931f3365cc56d0c36b09faa876
gpalmaj <gabrieljavaroni2@gmail.com>
2026-03-16 17:17:29 +0100

    movind test setup file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
+    setupFiles: ['./src/tests/setup.ts'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'text-summary', 'html'],
